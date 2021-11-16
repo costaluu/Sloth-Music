2d17ec00dbf83e2bd0c6a97446fa65e1171f5e27
jlcs3 <jlcs3@cin.ufpe.br>
2021-11-16 08:17:52 -0300

    refactor: put playnext in help command

--- src/Commands/common/help.ts
+++ src/Commands/common/help.ts
@@ -24,7 +24,7 @@ export const command: Command = {
                     },
                     {
                         name: `Music commands`,
-                        value: '`play`, `pause`, `resume`, `clear`, `search`, `queue`, `remove`, `seek`, `leave`, `skip`, `loop`, `jump`, `nowplaying`, `shuffle`, `fairshuffle`, `thread`',
+                        value: '`play`, `pause`, `resume`, `clear`, `search`, `queue`, `remove`, `seek`, `leave`, `skip`, `loop`, `jump`, `nowplaying`, `playnext`, `shuffle`, `fairshuffle`, `thread`',
                         inline: false,
                     },
                     {
